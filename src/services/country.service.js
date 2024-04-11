c9924d6c52314b114106435227e28febfa9abaa1
aHasnat1997 <hasnat56703@gmail.com>
2024-04-12 00:14:15 +0600

    ADD: button

--- src/services/country.service.js
+++ src/services/country.service.js
@@ -1,17 +1,29 @@
+import pkg from 'discord.js';
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageActionRow } = pkg;
+
 import axios from "axios";
 
 const restCountries = async (interaction) => {
   try {
-    // console.log(interaction);
     const countryName = interaction.options.get('country-name');
-    // console.log(countryName);
+
+    // const button = new ButtonBuilder()
+    //   .setCustomId('button')
+    //   .setLabel('Button')
+    //   .setStyle(ButtonStyle.Primary);
+
+    // const row = new ActionRowBuilder()
+    //   .addComponents(button);
 
     const countryData = await axios.get(`https://restcountries.com/v3.1/name/${countryName.value}`)
       .then(res => res.data)
       .catch(err => err)
     // console.log(countryData);
 
-    await interaction.reply(countryData[0].flags.png);
+    await interaction.reply({
+      content: countryData[0].flags.png,
+      // components: [buttonsRow]
+    });
 
   } catch (error) {
     await interaction.reply('🔴 No country found...');
