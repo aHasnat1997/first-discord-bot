515b5748ba493fce8ce0b5dd24383eadfbb052a6
aHasnat1997 <hasnat56703@gmail.com>
2024-04-13 23:26:35 +0600

    FIX: error

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,7 +1,7 @@
 import restCountries from "../services/country.service.js";
 import joke from "../services/joke.service.js";
 
-export default async function interactionCreateEvent(interaction) {
+export default function interactionCreateEvent(interaction) {
   // console.log(interaction);
   if (!interaction.isChatInputCommand()) return;
 
